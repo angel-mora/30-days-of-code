ef58fad2258c5db654db9cca8622ad38445fbdc3
jun-low <zhijun.low@gmail.com>
2019-10-29 11:37:55 +0800

    added day 23

---
+++ Day 23: BST Level-Order Traversal/23_solution.rb
@@ -0,0 +1,42 @@
+class Node
+    attr_accessor :left,:right,:data
+  def initialize data
+      @left=@right=nil
+      @data=data
+    end
+end
+class Solution
+    def insert (root,data)
+        if root==nil
+            return Node.new(data)
+        else
+            if data<=root.data
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else
+                cur=self.insert(root.right,data)
+                root.right=cur
+            end
+        end
+        return root
+    end
+
+    def levelOrder(root)
+    queue = [root]
+        while queue != 0
+            current = queue[0]
+            queue = queue[-1] # only return the first
+            print current.data.to_s
+        end
+            [levelOrder(root.left), levelOrder(root.right)] if root.nil?
+    end
+end
+
+myTree=Solution.new
+root=nil
+T=gets.to_i
+for i in 1..T
+    data=gets.to_i
+    root=myTree.insert(root,data)
+end
+myTree.levelOrder(root)
